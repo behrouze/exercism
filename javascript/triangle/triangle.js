416db2a82b9f921e417bd193dedd11c817c41a73
Beh <dev@behrouze.com>
2021-02-18 20:57:55 +0100

    triangle clean

--- javascript/triangle/triangle.js
+++ javascript/triangle/triangle.js
@@ -3,40 +3,38 @@ export class Triangle {
     this.sides = sides;
   }
 
-  sum(array){
-   return array.reduce((acc,x)=> acc+x ,0)
+  sum(array) {
+    return array.reduce((acc, x) => acc + x, 0);
   }
 
-  splice(index){
-    return this.sides.filter((x,i)=> i !== index)
+  splice(index) {
+    return this.sides.filter((x, i) => i !== index);
   }
 
-  numberOfDistinct(){
-    return [...new Set(this.sides)].length        
+  numberOfDistinct() {
+    return [...new Set(this.sides)].length;
   }
 
-  isTriangleInequalityOk(){
-    let xxx = this.sides.filter((side,idx)=>{
-      const otherSides = this.splice(idx)
-      return  this.sum(otherSides)  >= side
-    })
-    return xxx.length === 3
+  isTriangleInequalityOk() {
+    let xxx = this.sides.filter((side, idx) => {
+      const otherSides = this.splice(idx);
+      return this.sum(otherSides) >= side;
+    });
+    return xxx.length === 3;
   }
 
   get isEquilateral() {
-    if(!this.sides[0]) return false;
-    return this.numberOfDistinct() === 1
-    // return this.sides.reduce((acc,curr)=>  acc===curr ? acc:false ,this.sides[0]) ===this.sides[0]
-    // return this.side1 == this.side2 && this.side2 == this.side3
+    if (!this.sides[0]) return false;
+    return this.numberOfDistinct() === 1;
   }
 
   get isIsosceles() {
-    if(!this.isTriangleInequalityOk()) return false  
-    return this.numberOfDistinct()  <= 2
+    if (!this.isTriangleInequalityOk()) return false;
+    return this.numberOfDistinct() <= 2;
   }
 
   get isScalene() {
-    if(!this.isTriangleInequalityOk()) return false  
-    return this.numberOfDistinct()  === 3
-    }
+    if (!this.isTriangleInequalityOk()) return false;
+    return this.numberOfDistinct() === 3;
+  }
 }
