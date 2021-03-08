e85bd85b107693b0cb423bcf92b545e536341140
Beh <dev@behrouze.com>
2021-03-08 09:41:03 +0100

    etl with reducer

--- javascript/etl/etl.js
+++ javascript/etl/etl.js
@@ -1,11 +1,8 @@
 export const transform = (scoring) => {
-  const oldScores = Object.keys(scoring);
-  let newScores = {};
-
-  oldScores.map((score) => {
-    const letters = scoring[score];
-    letters.map((letter) => (newScores = { ...newScores, [letter.toLowerCase()]: Number(score) }));
-  });
-
-  return newScores;
+  return Object.keys(scoring).reduce((acc, score) => {
+    return scoring[score].reduce(
+      (acc2, letter) => (acc = { ...acc, [letter.toLowerCase()]: Number(score) }),
+      {}
+    );
+  }, {});
 };
